ef0fd55ea8a419467606232cab65a54e5471df4a
Ish200565 <ishikabelel@gmail.com>
2026-06-25 18:42:26 +0530

    Frontend connected

--- backend/app.py
+++ backend/app.py
@@ -10,7 +10,12 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    CORS(app)
+    CORS(app, origins=[
+        "https://transact-rank-1.onrender.com",
+        "http://127.0.0.1:5500",
+        "http://localhost:5500"
+    ])
+
     db.init_app(app)
     migrate.init_app(app, db)
 
